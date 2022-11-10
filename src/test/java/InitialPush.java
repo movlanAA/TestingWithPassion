c2917460313869cac67eca5db2311f5130141e5b
Movlan <87745618+movlanAA@users.noreply.github.com>
2022-11-09 18:59:00 -0800

    Update InitialPush.java
    whatsgood

--- src/test/java/InitialPush.java
+++ src/test/java/InitialPush.java
@@ -1,5 +1,8 @@
 public class InitialPush {
     public static void main(String[] args) {
         System.out.println("Testing for living");
+        
+        System.out.println("Javalama");
+    
     }
 }
